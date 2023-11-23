22b220d2308889c750dd53cda8b97aaf5344badf
Anatolii <ak0391@yandex.ru>
2023-11-24 01:54:01 +0300

    ---

--- task7.py
+++ task7.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import time
 
 # задаём опции открытия браузера
 options = Options()
@@ -14,7 +13,6 @@ driver = webdriver.Chrome(options=options)
 
 # Метод get открывает сайт по ссылке
 driver.get("http://localhost/litecart/en/")
-time.sleep(5)
 
 # список видимых кнопок
 list_tabs = driver.find_elements(By.CLASS_NAME, "image")
