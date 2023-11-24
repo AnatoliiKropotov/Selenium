23bfb815fd9266a7871680ac68517edd56ca75d7
Anatolii <ak0391@yandex.ru>
2023-11-24 11:50:54 +0300

    Добавил решение к заданию 7

--- task7.py
+++ task7.py
@@ -1,21 +1,47 @@
+import traceback
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-# задаём опции открытия браузера
-options = Options()
 
-# на весь экран
-options.add_argument("--start-maximized")
+def check_duck():
+    try:
+        # задаём опции драйвера
+        options = Options()
+        options.add_argument("--start-maximized")
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(3)
 
-# создаём объект driver
-driver = webdriver.Chrome(options=options)
+        driver.get("http://localhost/litecart/en/")
 
-# Метод get открывает сайт по ссылке
-driver.get("http://localhost/litecart/en/")
+        # находим всех уток
+        list_tabs = driver.find_elements(By.CLASS_NAME, "image-wrapper")
+        count_duck = len(list_tabs)
 
-# список видимых кнопок
-list_tabs = driver.find_elements(By.CLASS_NAME, "image")
-print(len(list_tabs))
-for i in list_tabs:
-    print(i.get_attribute("outerHTML"))
+        count_one_sticker_duck = 0
+        for i in list_tabs:
+            list_elements = i.find_elements(By.TAG_NAME, "div")
+
+            # проверяем что существует один стикер на каждой утке
+            if len(list_elements) == 1:
+                for j in list_elements:
+
+                    # проверяем что это именно нужный стикер
+                    if j.get_attribute("class") in ("sticker new", "sticker sale"):
+                        count_one_sticker_duck += 1
+
+        # условие на вывод
+        if count_duck == count_one_sticker_duck:
+            print(f"У каждой утки по одному нужному стикеру. Всего уток {count_duck}")
+        else:
+            print("Не у каждой утки нужное количество стикеров")
+        driver.quit()
+        return
+
+    except Exception:
+        print("Возникла ошибка: ", traceback.format_exc())
+        driver.quit()
+        return
+
+
+check_duck()
