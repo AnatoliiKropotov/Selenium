085947deee0d94063aa0c58994b829637485ffab
Anatolii <ak0391@yandex.ru>
2023-11-28 16:41:44 +0300

    Полностью рабочий вариант для Chrome

--- task10.py
+++ task10.py
@@ -23,7 +23,7 @@ def check_price(main_page_price, inner_page_price, type_price):
 # в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой,
 # у которого в RGBa представлении одинаковые значения для каналов R, G и B)
 def check_price_style_regular(regular_price_color, regular_price_style, page):
-    if regular_price_color == "rgba(119, 119, 119, 1)" and regular_price_style == 's':
+    if regular_price_color[0] == regular_price_color[1] == regular_price_color[2] and regular_price_style == 's':
         print(f"{page}: цвет обычной цены серый, стиль перечёркнутый: ", True)
     else:
         print(f"{page}: цвет обычной цены серый, стиль перечёркнутый: ", False)
@@ -32,7 +32,7 @@ def check_price_style_regular(regular_price_color, regular_price_style, page):
 # г) акционная цена жирная и красная (можно считать, что "красный" цвет это такой,
 # у которого в RGBa представлении каналы G и B имеют нулевые значения)
 def check_price_style_campaign(campaign_price_color, campaign_price_style, page):
-    if campaign_price_color == "rgba(204, 0, 0, 1)" and campaign_price_style == 'strong':
+    if campaign_price_color[1] == campaign_price_color[2] == 0 and campaign_price_style == 'strong':
         print(f"{page}: цвет акционной цены красный, стиль жирный: ", True)
     else:
         print(f"{page}: цвет обычной цены красный, стиль жирный: ", False)
@@ -47,9 +47,15 @@ def check_price_size(regular_price_height, regular_price_width, campaign_price_h
         print(f"{page}: акционная цена крупнее, чем обычная:", False)
 
 
+# переводим цвет в кортеж
+def color_to_tuple(color):
+    color_str = color.replace("rgba", "").strip("()")
+    color = tuple(int(item) for item in color_str.split(','))
+    return color
+
 
 # получения значений товара
-def func():
+def main():
     try:
         options = Options()
         options.add_argument("--start-maximized")
@@ -62,7 +68,6 @@ def func():
 
         # так как нам нужен первый товар, используем find_element
         duck_name = block_campaigns.find_element(By.CLASS_NAME, "name").text
-
         regular_price = block_campaigns.find_element(By.CLASS_NAME, "regular-price").text
         regular_price_color = block_campaigns.find_element(By.CLASS_NAME, "regular-price").value_of_css_property(
             "color")
@@ -79,6 +84,7 @@ def func():
         campaign_price_width = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").get_property(
             "offsetWidth")
 
+        # для наглядности выводим найденные параметры
         main_page_values = (duck_name, regular_price, regular_price_color, regular_price_style,
                             campaign_price, campaign_price_color, campaign_price_style, regular_price_height,
                             regular_price_width, campaign_price_height, campaign_price_width)
@@ -108,6 +114,7 @@ def func():
         campaign_price_width_inner = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").get_property(
             "offsetWidth")
 
+        # для наглядности выводим найденные параметры
         values_inner = (duck_name_inner, regular_price_inner, regular_price_color_inner, regular_price_style_inner,
                         campaign_price_inner, campaign_price_color_inner, campaign_price_style_inner,
                         regular_price_height_inner, regular_price_width_inner, campaign_price_height_inner,
@@ -117,21 +124,19 @@ def func():
         # делаем проверки
         # название
         check_name(duck_name, duck_name_inner)
-
         # значение цены
         type_price = "обычная"
         check_price(regular_price, regular_price_inner, type_price)
         type_price = "акционная"
         check_price(campaign_price, campaign_price_inner, type_price)
-
-        # цвет и стиль цены
+        # цвет, стиль, размер цены
         page = "Главная страница"
-        check_price_style_regular(regular_price_color, regular_price_style, page)
-        check_price_style_campaign(campaign_price_color, campaign_price_style, page)
+        check_price_style_regular(color_to_tuple(regular_price_color), regular_price_style, page)
+        check_price_style_campaign(color_to_tuple(campaign_price_color), campaign_price_style, page)
         check_price_size(regular_price_height, regular_price_width, campaign_price_height, campaign_price_width, page)
         page = "Карточка товара"
-        check_price_style_regular(regular_price_color_inner, regular_price_style_inner, page)
-        check_price_style_campaign(campaign_price_color_inner, campaign_price_style_inner, page)
+        check_price_style_regular(color_to_tuple(regular_price_color_inner), regular_price_style_inner, page)
+        check_price_style_campaign(color_to_tuple(campaign_price_color_inner), campaign_price_style_inner, page)
         check_price_size(regular_price_height_inner, regular_price_width_inner, campaign_price_height_inner,
                          campaign_price_width_inner, page)
 
@@ -143,4 +148,4 @@ def func():
         return
 
 
-func()
+main()
