f77c20065a90f6c2f9268c1915b35082d4c318c5
Anatolii <ak0391@yandex.ru>
2023-11-28 13:50:47 +0300

    123

--- task10.py
+++ task10.py
@@ -17,22 +17,34 @@ def func():
         driver.get("http://localhost/litecart/en/")
 
         block_campaigns = driver.find_element(By.ID, "box-campaigns")
-        print(block_campaigns.get_attribute("outerHTML"))
+        # print(block_campaigns.get_attribute("outerHTML"))
 
-        #a
+        # так как нам нужен первый товар, используем find_element
         duck_name = block_campaigns.find_element(By.CLASS_NAME, "name").text
         manufacture_name = block_campaigns.find_element(By.CLASS_NAME, "manufacturer").text
 
         regular_price = block_campaigns.find_element(By.CLASS_NAME, "regular-price").text
-        regular_price_color = block_campaigns.find_element(By.CLASS_NAME, "regular-price").value_of_css_property("color")
+        regular_price_color = block_campaigns.find_element(By.CLASS_NAME, "regular-price").value_of_css_property(
+            "color")
         regular_price_style = block_campaigns.find_element(By.CLASS_NAME, "regular-price").tag_name
 
+        regular_price_height = block_campaigns.find_element(By.CLASS_NAME, "regular-price").get_property("offsetHeight")
+        regular_price_width = block_campaigns.find_element(By.CLASS_NAME, "regular-price").get_property(
+            "offsetWidth")
+
         campaign_price = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").text
-        campaign_price_color = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").value_of_css_property("color")
+        campaign_price_color = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").value_of_css_property(
+            "color")
         campaign_price_style = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").tag_name
 
+        campaign_price_height = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").get_property(
+            "offsetHeight")
+        campaign_price_width = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").get_property(
+            "offsetWidth")
+
         main_page_values = (duck_name, manufacture_name, regular_price, regular_price_color, regular_price_style,
-                            campaign_price, campaign_price_color, campaign_price_style)
+                            campaign_price, campaign_price_color, campaign_price_style, regular_price_height,
+                            regular_price_width, campaign_price_height, campaign_price_width)
         print(main_page_values)
         # print(duck_name)
         # print(manufacture_name)
@@ -42,11 +54,22 @@ def func():
         # print(campaign_price)
         # print(campaign_price_color)
         # print(campaign_price_style)
+        # print(regular_price_height)
+        # print(regular_price_width)
+        # print(campaign_price_height)
+        # print(campaign_price_width)
 
 
-        #переходим внуть товара
+        # переходим внуть товара
         block_campaigns.find_element(By.CLASS_NAME, "listing-wrapper.products").find_element(By.TAG_NAME, 'a').click()
-        time.sleep(3)
+
+        block_campaigns = driver.find_element(By.ID, "box-product")
+        print(block_campaigns.get_attribute("outerHTML"))
+        regular_price_inner = block_campaigns.find_element(By.CLASS_NAME, "regular-price").text
+        campaign_price_inner = block_campaigns.find_element(By.CLASS_NAME, "campaign-price").text
+        print(regular_price_inner)
+        print(campaign_price_inner)
+
         # print(block_campaigns.get_attribute("outerHTML"))
 
 
