8217d30365b0dc5795e59d75a09b9f0148136320
Anatolii <ak0391@yandex.ru>
2023-12-04 16:50:33 +0300

    Убрал лишний шаг (первый цикл)

--- task7.py
+++ task7.py
@@ -13,20 +13,17 @@ def check_sticker():
 
         driver.get("http://localhost/litecart/en/")
 
-        product_bloks_list = driver.find_elements(By.CLASS_NAME, 'listing-wrapper.products')
         count_duck = 0
         count_sticker = 0
-        for i in range(0, len(product_bloks_list)):
-            products_list = product_bloks_list[i].find_elements(By.CLASS_NAME, "product")
-            for j in range(0, len(products_list)):
-                if len(products_list[j].find_elements(By.CSS_SELECTOR, "[class^='sticker']")) == 1:
-                    count_sticker += 1
-                count_duck += 1
+        products_list = driver.find_elements(By.CLASS_NAME, "product")
+        for j in range(0, len(products_list)):
+            if len(products_list[j].find_elements(By.CSS_SELECTOR, "[class^='sticker']")) == 1:
+                count_sticker += 1
+            count_duck += 1
         if count_duck == count_sticker:
             print("У каждого товара по одному стикеру: ", True)
         else:
             print("У каждого товара по одному стикеру: ", False)
-
         driver.quit()
         return
 
