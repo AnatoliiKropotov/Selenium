3ddd3605efe7e0bad7541b25b9c59eff6ee6ab1d
Anatolii <ak0391@yandex.ru>
2023-12-12 19:25:49 +0300

    Доделал

--- task17.py
+++ task17.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 options = Options()
 options.add_argument("--start-maximized")
 driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(1)
+driver.implicitly_wait(3)
 
 driver.get("http://localhost/litecart/admin/")
 
@@ -22,51 +22,23 @@ driver.find_element(By.XPATH, '//span[text()="Catalog"]').click()
 
 driver.find_element(By.XPATH, '//a[text()="Rubber Ducks"]').click()
 
-# text_colspan = driver.find_element(By.CSS_SELECTOR, '[colspan="5"]').text
+# text_colspan = driver.find_element(By.CSS_SELECTOR, '[colspan="5"]').textdr
 
+driver.find_element(By.LINK_TEXT, "Subcategory").click()
+x = driver.find_elements(By.XPATH, '//a[contains(text(), "Duck") and not(contains(text(), "Ducks"))]')
 
-# x = driver.find_elements(By.XPATH, '//a[contains(text(), "Duck") and not(contains(text(), "Ducks"))]')
-# print(len(x))
-# len_list = len(x)
-# for i in x:
-#     print(i.get_attribute('outerHTML'))
+count = 0
+for i in range(0, len(x)):
+    x[i].click()
 
-string_colspan = driver.find_element(By.CSS_SELECTOR, '[colspan="5"]').text
-list_strings = string_colspan.split('|')
-count_categories = int(list_strings[0].replace('Categories: ',''))
-count_products = int(list_strings[1].replace('Products: ',''))
-print("count_products: ", count_products)
+    # выводим логи если массив не пустой
+    logs = driver.get_log("browser")
+    if logs:
+        for log in logs:
+            print(log)
 
-category_number = 1
-while category_number <= count_categories:
-    driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=" + str(category_number))
-    if category_number != 1:
-        driver.find_element(By.LINK_TEXT, "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=" + str(category_number)).click()
-        list_strings= driver.find_element(By.CSS_SELECTOR, '[colspan="5"]').text.split('|')
-        count_products_new = int(list_strings[1].replace('Products: ', ''))
-        print("count_products_new:", count_products_new)
-        if count_products_new - count_products == 0:
-            category_number += 1
-            continue
-        else:
-            count_products = count_products_new - count_products
-
-
-    count = 1
-    while count != count_products:
-        print('1')
-        print(driver.find_element(By.LINK_TEXT, "http://localhost/litecart/admin/?app=catalog&doc=edit_product&category_id=" + str(category_number) + "&product_id=" + str(count)))
-        driver.find_element(By.LINK_TEXT, "http://localhost/litecart/admin/?app=catalog&doc=edit_product&category_id=" + str(category_number) + "&product_id=" + str(count)).click()
-        logs = driver.get_log("browser")
-
-        # выводим логи если массив не пустой
-        if logs:
-            for log in logs:
-                print(log)
-
-        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=" + str(category_number))
-        # x = driver.find_elements(By.XPATH, '//a[contains(text(), "Duck") and not(contains(text(), "Ducks"))]')
-        count += 1
-    category_number += 1
+    driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=2")
+    x = driver.find_elements(By.XPATH, '//a[contains(text(), "Duck") and not(contains(text(), "Ducks"))]')
+    count += 1
 
 driver.quit()
